f04a8f861eed00f416c9d327fd092a061e71b69a
aliramirez08 <aliramirez08@aol.com>
2025-07-19 14:58:23 -0400

    Initial commit for CSC450 Module 1 assignment

---
+++ fixed_syntax_part2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Standard Output Statement
+    cout << "Welcome to this C++ Program" << endl;
+
+    cout << "I have corrected all errors for this program." << endl;
+
+    return 0;
+}
---
+++ fixed_syntax_part3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double myMoney = 1000.50; // this should be printed out
+
+    // Standard Output Statement
+    cout << "Please be sure to correct all syntax errors in this program" << endl;
+
+    cout << "I have corrected all errors for this program." << endl;
+
+    cout << "The total amount of money available is = " << myMoney << endl;
+
+    return 0;
+}
